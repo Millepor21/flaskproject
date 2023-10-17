58700a5196e3b3c67ef812df3c4919e04e291f12
Millepor21 <millepor21@gmail.com>
2023-10-17 15:55:49 -0400

    finished appointments routes

--- resources/appointments/routes.py
+++ resources/appointments/routes.py
@@ -1,13 +1,14 @@
 from flask.views import MethodView
 from flask_smorest import abort
 from flask_jwt_extended import jwt_required, get_jwt_identity
+from psycopg2 import IntegrityError
 from schemas import AppointmentsSchema
 from . import bp 
 from .AppointmentsModel import AppointmentModel
 from resources.doctors.DoctorModel import DoctorModel
 from resources.patients.PatientModel import PatientModel
 
-@bp.route('/Appointments')
+@bp.route('/appointments')
 class AppointmentList(MethodView):
 
     @jwt_required()
@@ -23,12 +24,43 @@ class AppointmentList(MethodView):
         else:
             abort(400, message='No account found')
 
-    
-    def post(self):
-        pass
+    @jwt_required()
+    @bp.arguments(AppointmentsSchema)
+    @bp.response(201, AppointmentsSchema)
+    def post(self, appointment_data):
+        patient_id = get_jwt_identity()
+        a = AppointmentModel(**appointment_data, patient_id = patient_id)
+        try:
+            a.save()
+            return a
+        except IntegrityError:
+            abort(400,message="Invalid User Id")
 
-    def put(self):
-        pass
 
-    def delete(self):
-        pass
+@bp.route('/appointments/<appointment_id>')
+class Apointment(MethodView):
+
+    @jwt_required()
+    @bp.arguments(AppointmentsSchema)
+    @bp.response(200, AppointmentsSchema)
+    def put(self, appointment_data, appointment_id):
+        a = AppointmentModel.query.get(appointment_id)
+        if a and appointment_data['body']:
+            patient_id = get_jwt_identity()
+            if a.patient_id == patient_id:
+                a.body = appointment_data['body']
+                a.save()
+                return a
+            else:
+                abort(401, message="Unauthorized")
+        abort(400, message="Invalid Post Data")
+    @jwt_required()
+    def delete(self, appointment_id):
+        patient_id = get_jwt_identity()
+        a = AppointmentModel.query.get(appointment_id)
+        if a:
+            if a.patient_id == patient_id:
+                a.delete()
+                return {'message': 'Appointment Canceled'}, 202
+            abort(401, message="User doesn't have rights")
+        abort(400, message="Invalid Appointment Id")
